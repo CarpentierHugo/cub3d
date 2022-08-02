c0b01a2852d9d209b7b92af34ed3428cc21174a4
CarpentierHugo <86190807+CarpentierHugo@users.noreply.github.com>
2022-08-02 03:29:54 +0200

    Update cub3d.h

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achatela <achatela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hcarpent <hcarpent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 21:41:45 by hcarpent          #+#    #+#             */
-/*   Updated: 2022/07/28 15:22:33 by achatela         ###   ########.fr       */
+/*   Updated: 2022/08/02 03:24:19 by hcarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@
 #define Q 0
 #define S 1
 #define D 2
+#define L_ARROW 123
+#define R_ARROW 124
 #define ESC 53
 #define PI 3.1415926535
-#define RES 128
+#define DR PI / 180
+#define SQR_SIZE 64
 #define FOV 60
-#define MOV_SPD 8
+#define MOV_SPD 10
 #define ROT_SPD 0.2
+#define SCREEN_W 320
+#define SCREEN_H 8 * FOV
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -32,15 +37,28 @@
 #include <math.h>
 #include "mlx/mlx.h"
 
+typedef struct  s_img
+{
+    void    *ptr;
+    int     *data;
+    int     h;
+    int     w;
+    int     bpp;
+    int     sl;
+    int     e;
+}   t_img;
+
 typedef struct  s_glob
 {
     char    **map;
     void	*mlx_ptr;
 	void	*win_ptr;
+    t_img   n_img[1];
+    t_img   s_img[1];
+    t_img   e_img[1];
+    t_img   w_img[1];
     float   px;
     float   py;
-    float   pdx;
-    float   pdy;
     float   pa;
 }   t_glob;
 
