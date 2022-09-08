e3a63ff3b3243617cae5dc91075f0c81bf2967a9
Alexis CHATELAIN <achatela@e2r3p5.clusters.42paris.fr>
2022-09-08 20:49:08 +0200

    point h normé

--- cub3d/cub3d.h
+++ cub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: achatela <achatela@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 21:41:45 by hcarpent          #+#    #+#             */
-/*   Updated: 2022/09/08 19:54:41 by achatela         ###   ########.fr       */
+/*   Updated: 2022/09/08 20:48:55 by achatela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,101 +33,102 @@
 #  define ESC       53
 # endif
 
-#define PI 3.1415926535
-#define DR PI / 180
-#define SQR 64
-#define RES 32
-#define FOV 60
-#define MOV_SPD SQR / 10
-#define ROT_SPD 0.05
-#define SCREEN_H 800
-#define SCREEN_W 1200
+# define PI 3.1415926535
+# define DR 0.01745329251
+# define SQR 64
+# define RES 32
+# define FOV 60
+# define MOV_SPD 6.4
+# define ROT_SPD 0.05
+# define SCREEN_H 800
+# define SCREEN_W 1200
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "mlx/mlx.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include "mlx/mlx.h"
 
-typedef struct  s_img
+typedef struct s_img
 {
-    char    *path_texture;
-    void    *ptr;
-    int     *data;
-    int     h;
-    int     w;
-    int     bpp;
-    int     sl;
-    int     e;
-}   t_img;
+	char	*path_texture;
+	void	*ptr;
+	int		*data;
+	int		h;
+	int		w;
+	int		bpp;
+	int		sl;
+	int		e;
+}	t_img;
 
-typedef struct  s_glob
+typedef struct s_glob
 {
-    int     minimap;
-    int     infinite;
-    char    **map;
-    int     *data;
-    void    *image;
-    char    **free_map;
-    void	*mlx_ptr;
-	void	*win_ptr;
-    t_img   n_img[1];
-    t_img   s_img[1];
-    t_img   e_img[1];
-    t_img   w_img[1];
-    int     map_begin;
-    long long int     floor;
-    long long int     ceiling;
-    float   px;
-    float   py;
-    float   pa;
-    int     ispos;
-    int     isone;
-    int     iszero;
-    float   ry;
-    float   rx;
-    float   ra;
-    int     length;
-}   t_glob;
+	int					minimap;
+	int					infinite;
+	char				**map;
+	int					*data;
+	void				*image;
+	char				**free_map;
+	void				*mlx_ptr;
+	void				*win_ptr;
+	t_img				n_img[1];
+	t_img				s_img[1];
+	t_img				e_img[1];
+	t_img				w_img[1];
+	int					map_begin;
+	long long int		floor;
+	long long int		ceiling;
+	float				px;
+	float				py;
+	float				pa;
+	int					ispos;
+	int					isone;
+	int					iszero;
+	float				ry;
+	float				rx;
+	float				ra;
+	int					length;
+}	t_glob;
 
-int    ft_get_textures(t_glob *glob, int i);
-char    **ft_split_modif(char *str, char c, int i, int k);
+int				ft_get_textures(t_glob *glob, int i);
+char			**ft_split_modif(char *str, char c, int i, int k);
 long long int	ft_atoi(const char *nptr);
-void    ft_minimap(t_glob *glob);
-void    ft_move(t_glob *glob, int key);
-void    ft_free(t_glob *glob);
-void    ft_verif_map(t_glob *glob);
-void    ft_raycasting(t_glob *glob);
-int	ft_north_texture(t_glob *glob, char *texture, int length);
-int	ft_south_texture(t_glob *glob, char *texture, int length);
-int	ft_west_texture(t_glob *glob, char *texture, int length);
-int	ft_east_texture(t_glob *glob, char *texture, int length);
-int	ft_rgb_error(char **number);
-void	ft_free_map(t_glob *glob);
-int	check_direction(char first, char second);
-int	ft_check_integer(char **number);
-int	ft_rgb_error2(char **number);
-int	ft_strlen_custom(char *str);
-int	ft_get_ceiling(t_glob *glob, char *texture);
-int	ft_get_floor(t_glob *glob, char *texture);
-int	ft_path_texture(t_glob *glob, char direction, int j, char *texture);
-int	ft_map_beginning(char *str);
-int	ft_str_is_beginning(char *str);
-void	ft_free_number(char **number, int i, t_glob *glob, int index);
-void	first_line(t_glob *glob, int test);
-void	second_line(t_glob *glob, int test);
-void	third_line(t_glob *glob, int test);
-void	forth_line(t_glob *glob, int test, int k);
-void	fifth_line(t_glob *glob, int test, int k);
-int	ft_strlen(char *str);
-void	ft_draw_square(t_glob *glob, int posx, int posy, int color);
-int	ft_screen(t_glob *glob);
-int	ft_exit(t_glob *glob);
-int	ft_mouse(int button, int x, int y, void *param);
-void	ft_draw_player(t_glob *glob, int size, int color);
-int	ft_deal_key(int key, void *param);
-int	ft_count_words(char *str, char c);
+void			ft_minimap(t_glob *glob);
+void			ft_move(t_glob *glob, int key);
+void			ft_free(t_glob *glob);
+void			ft_verif_map(t_glob *glob);
+void			ft_raycasting(t_glob *glob);
+int				ft_north_texture(t_glob *glob, char *texture, int length);
+int				ft_south_texture(t_glob *glob, char *texture, int length);
+int				ft_west_texture(t_glob *glob, char *texture, int length);
+int				ft_east_texture(t_glob *glob, char *texture, int length);
+int				ft_rgb_error(char **number);
+void			ft_free_map(t_glob *glob);
+int				check_direction(char first, char second);
+int				ft_check_integer(char **number);
+int				ft_rgb_error2(char **number);
+int				ft_strlen_custom(char *str);
+int				ft_get_ceiling(t_glob *glob, char *texture);
+int				ft_get_floor(t_glob *glob, char *texture);
+int				ft_path_texture(t_glob *glob, char direction,
+					int j, char *texture);
+int				ft_map_beginning(char *str);
+int				ft_str_is_beginning(char *str);
+void			ft_free_number(char **number, int i, t_glob *glob, int index);
+void			first_line(t_glob *glob, int test);
+void			second_line(t_glob *glob, int test);
+void			third_line(t_glob *glob, int test);
+void			forth_line(t_glob *glob, int test, int k);
+void			fifth_line(t_glob *glob, int test, int k);
+int				ft_strlen(char *str);
+void			ft_draw_square(t_glob *glob, int posx, int posy, int color);
+int				ft_screen(t_glob *glob);
+int				ft_exit(t_glob *glob);
+int				ft_mouse(int button, int x, int y, void *param);
+void			ft_draw_player(t_glob *glob, int size, int color);
+int				ft_deal_key(int key, void *param);
+int				ft_count_words(char *str, char c);
 
 #endif
